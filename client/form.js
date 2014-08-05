1d11005844f72368f2b21a3c26a2455a630613d1
claudeaubry <claude.aubry@gmail.com>
2014-08-05 16:43:45 +0200

    nav bar

--- client/form.js
+++ client/form.js
@@ -1,10 +1,11 @@
-Template.form.events({
+Template.formBook.events({
   'submit form': function (event) {
     var book = $("input#bookSubmit").val();
     var author = $("input#authorSubmit").val();
-    
+
     event.preventDefault();
     Books.insert({title: book, author: author, statut: "proposed"});
     $("input#authorSubmit").val("");
+    $("input#bookSubmit").val("");
   }
 });

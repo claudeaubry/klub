fa95cd345e6b1a58183163790ce1ec85d68e8e10
claudeaubry <claude.aubry@gmail.com>
2014-08-18 08:59:55 +0200

    add remove book in proposal

--- client/proposal.js
+++ client/proposal.js
@@ -1,3 +1,7 @@
 Template.proposal.titles = function(){
   return Books.find({statut : "proposed"});
 };
+
+Template.proposal.events = {
+  'click .deleteBook': function(){ return Books.remove(this._id)}
+}

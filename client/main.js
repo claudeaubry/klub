e4e4bc68d71542cc8bff5a45cc3049c5926cbeca
Pntbr <pntbr@scopyleft.fr>
2014-09-02 17:57:44 +0200

    Remove autopublish and install publish/subscribe system

--- client/main.js
+++ client/main.js
@@ -1,4 +1,8 @@
 Meteor.startup(function () {
   $('body').scrollspy({target: '.navbar-default'});
   document.title = klubLocalDatas.title;
+  
+  Meteor.subscribe("all_books");
+  Meteor.subscribe("all_ksessions");
+  Meteor.subscribe("all_users");
 });
--- server/server.js
+++ server/server.js
@@ -1,10 +1,4 @@
 Meteor.startup(function () {
-  // Describe the link between Collections and JSON files
-  schema = [
-    {collection: Books, jsonKey: 'books'},
-    {collection: KSessions, jsonKey: 'ksessions'},
-  ];
-
   // Reset Database only in environment dev
   if (process.env.NODE_ENV === 'development') {
     KSessions.remove({});
@@ -15,19 +9,34 @@ Meteor.startup(function () {
     datas = Datas; // Specific datas for prod environment
   }
 
-  // Insert all datas describes in schema
-  schema.forEach(function (table) {
-    datas[table.jsonKey].forEach(function(record) {
-      if (table.collection.findOne(record._id)) {
-        table.collection.update(record._id, record);
-      }
-      else {
-        table.collection.insert(record);
-      }
-
-    });
+  // Specific insert for Books
+  datas.books.forEach(function(record) {
+    if (Books.findOne(record._id)) {
+      Books.update(record._id, record);
+    }
+    else {
+      Books.insert(record);
+    }
   });
-
+  
+  Meteor.publish("all_books", function() {
+    return Books.find();
+  });
+  
+  // Specific insert for KSessions
+  datas.ksessions.forEach(function(record) {
+    if (KSessions.findOne(record._id)) {
+      KSessions.update(record._id, record);
+    }
+    else {
+      KSessions.insert(record);
+    }
+  });
+  
+  Meteor.publish("all_ksessions", function() {
+    return KSessions.find();
+  });
+  
   // Specific insert for users
   datas.users.forEach(function(user) {
     if (! Meteor.users.findOne({username: user.username})) {

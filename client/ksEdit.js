7dfe4d6e838407c7ddc06bbe143660571a38957b
claudeaubry <claude.aubry@gmail.com>
2014-09-25 19:27:46 +0200

    create ksession and book select

--- client/ksEdit.js
+++ client/ksEdit.js
@@ -1,22 +1,22 @@
 Template.ksEdit.helpers({
   ks: function () {
-    return this;
+    var ks = this;
+    ks.books = Books.find();
+    ks.book = Books.findOne(ks.book_id) ? Books.findOne(ks.book_id).title : "";
+    
+    return ks;
   }
 });
 
 Template.ksEdit.events({
   'submit form': function (elt) {
-    var _id = $('input.id').val();
-    var ks = {};
+    var _id = $('input.id').val(),
+      ks = _id ? KSessions.findOne(_id) : {};
 
     elt.preventDefault();
+    ks.i = +$("input.i").val();
+    ks.klubMaster = $("input.klubMaster").val();
 
-    if (_id) {
-      ks = KSessions.findOne(_id);
-      ks.i = +$("input.i").val();
-      ks.klubMaster = $("input.klubMaster").val();
-      KSessions.update(_id, ks);
-    }
-
+    _id ? KSessions.update(_id, ks) : KSessions.insert(ks);
   }
-})
+});

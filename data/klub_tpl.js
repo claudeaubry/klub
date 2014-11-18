395bbf36a54d3258473298fd7ea2c402d859928b
claudeaubry <claude.aubry@gmail.com>
2014-11-18 17:45:42 +0100

    update to meteor 1.0

--- data/klub_tpl.js
+++ data/klub_tpl.js
@@ -13,11 +13,11 @@ klubLocalDatas = {
   hour : "18h30-20h"
 };
 klubAppDatas = {
-    uvp: "Une application qui s'adresse aux participants de Klub, pour préparer et prolonger le karma des séances de partage sur un livre.",
-    actors: "Le klubOwner prépare un Klub en s'occupant de la sélection du livre, le klubMaster est chargé du compte-rendu, et tout le monde peut proposer des livres.",
-    license : "Klub est un logiciel libre, sous licence MIT. Les contributeurs sont les bienvenus",
-    framework : "Meteor, un framework open-source de développement web en Javascript basé sur Node.js",
-    feedback: "Vous pouvez poser une question, proposer une évolution ou laisser un petit message d'encouragement au développeur principal",
-    mail : "claude.aubry@gmail.com",
-    github: "https://github.com/claudeaubry/klub"
+  uvp: "Une application qui s'adresse aux participants de Klub, pour préparer et prolonger le karma des séances de partage sur un livre.",
+  actors: "Le klubOwner prépare un Klub en s'occupant de la sélection du livre, le klubMaster est chargé du compte-rendu, et tout le monde peut proposer des livres.",
+  license : "Klub est un logiciel libre, sous licence MIT. Les contributeurs sont les bienvenus",
+  framework : "Meteor, un framework open-source de développement web en Javascript basé sur Node.js",
+  feedback: "Vous pouvez poser une question, proposer une évolution ou laisser un petit message d'encouragement au développeur principal",
+  mail : "claude.aubry@gmail.com",
+  github: "https://github.com/claudeaubry/klub"
 };

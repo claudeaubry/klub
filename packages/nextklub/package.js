60b47e233d400ba62505b0c60ad865711ad63852
claudeaubry <claude.aubry@gmail.com>
2014-12-09 14:59:26 +0100

    create structure package for next session

---
+++ packages/nextklub/package.js
@@ -0,0 +1,22 @@
+Package.describe({
+  summary: "The next Klub"
+});
+
+Package.on_use(function (api, where) {
+  api.use(['templating'], 'client');
+  api.add_files([
+    'lib/client/main.js'
+    ],
+    'client');
+    api.add_files(['lib/server/server.js'], ['server']);
+    api.add_files(['lib/collections/next_klub.js'], ['client', 'server']);
+
+    if (api.export) {
+      api.export(['NextKlub']);
+    }
+  });
+
+  Package.on_test(function (api) {
+    api.use(['nextklub', 'tinytest', 'test-helpers'], 'client');
+    api.add_files('next_klub_test.js', 'client');
+  });

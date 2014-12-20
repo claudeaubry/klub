9366568f15ca3bf07a2e251cb7b1a3bc1977bbe4
claudeaubry <claude.aubry@gmail.com>
2014-12-20 11:12:46 +0100

    remove unuseful variable for book

--- packages/book/lib/client/book_create.js
+++ packages/book/lib/client/book_create.js
@@ -1,6 +1,5 @@
 Template.bookCreate.events({
   'click .createBook': function () {
-    $("#_idSubmit").val('');
     $("input#bookSubmit").val('');
     $("input#authorSubmit").val('');
     $("input#teaserSubmit").val('');
@@ -8,10 +7,9 @@ Template.bookCreate.events({
   },
 
   'submit form': function (event) {
-    var _id = $("#_idSubmit").val(),
-    book = $("input#bookSubmit").val(),
-    author = $("input#authorSubmit").val(),
-    teaser = $("input#teaserSubmit").val(),
+    book = $("input#bookSubmit").val();
+    author = $("input#authorSubmit").val();
+    teaser = $("input#teaserSubmit").val();
     origin = $("input#originSubmit").val();
 
     event.preventDefault();

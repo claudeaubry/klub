f4e19c7f93ef436214160c02576e64a21fb586fe
claudeaubry <claude.aubry@gmail.com>
2015-01-30 11:41:47 +0100

    remove un console.log

--- packages/pastklub/past_klub_test.js
+++ packages/pastklub/past_klub_test.js
@@ -16,18 +16,14 @@ Tinytest.add("futureI with no klub", function (test) {
   removeAll();
 });
 
-Tinytest.add("futureI with klubs an max i equal to 13", function (test) {
+Tinytest.add("futureI with klubs with a max i equal to 13", function (test) {
   var i = 5;
 
   PastKlub.insert({i: 1});
   PastKlub.insert({i: 5});
   PastKlub.insert({i: 13});
 
-  console.log("before", PastKlub.find().count())
-
   test.equal(futureI(),14);
 
   removeAll();
 });
-
-

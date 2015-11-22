a7088417a7e339b1254dd5dc3b3c19fce6effc69
claudeaubry <claude.aubry@gmail.com>
2015-11-22 21:24:27 +0100

    ajouté un attribut où est conservé le lien photo

--- packages/pastklub/lib/client/past_klub_modal.js
+++ packages/pastklub/lib/client/past_klub_modal.js
@@ -9,6 +9,7 @@ Template.formKM.events({
 
     event.preventDefault();
     klubTarget.url = $("input#linkSubmit").val();
+    klubTarget.pic = $("input#picSubmit").val();
     klubTarget.summary = $("textarea#summarySubmit").val();
     klubTarget.attendees = $("input#attendeesSubmit").val().split(",");
     PastKlub.update(klubTarget._id, klubTarget);

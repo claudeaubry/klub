02286a8bda98e7d9018857e68bb39e292e77c943
claudeaubry <claude.aubry@gmail.com>
2016-02-11 18:24:07 +0100

    new intro to klubs

--- data/klub_tpl.js
+++ data/klub_tpl.js
@@ -1,19 +1,20 @@
 klubDatas = {
-  principle : "Les participants lisent un livre défini à l'avance et viennent en discuter ensemble lors d'un Klub.",
-  objective : "Ils mettent en commun leurs impressions, leurs opinions, leurs connaissances et leurs expériences sur le sujet du livre. Un klubMaster, qui peut changer à chaque fois parmi les participants, facilite le déroulement du Klub.",
-  rule : "Pour participer à un Klub, il faut avoir lu le livre au préalable.",
-  theme : "Des livres en rapport, directement ou indirectement, avec l'Agilité.",
-  slogan: "Klub, le bon karma de la lecture partagée.",
+  workflow : "Proposition de sujets -> Choix d'un sujet -> Inscriptions -> Rencontre (le klub !) -> Résumé.",
+//  objective : "Ils mettent en commun leurs impressions, leurs opinions, leurs connaissances et leurs expériences sur le sujet du livre. Un klubMaster, qui peut changer à chaque fois parmi les participants, facilite le déroulement du Klub.",
+//  rule : "Pour participer à un Klub, il faut avoir lu le livre au préalable.",
+  theme : "Des sujets en rapport, directement ou indirectement, avec l'Agilité.",
+  types : "Klub pour la lecture, Skrub pour le scribing, Kjeub pour le jeu.",
+  valueProposition : "Discuter d'un livre, dessiner sur un sujet, jouer ensemble : klub, le bon karma du partage."
 };
 klubLocalDatas = {
   region : "Toulouse",
-  title : "Klub de lecture Agile Toulouse",
+  title : "Klubs Agile Toulouse",
   place: "Maison des Associations",
   map: "http://www.openstreetmap.org/way/63969787#map=16/43.5775/1.4442",
   hour : "18h30-20h"
 };
 klubAppDatas = {
-  uvp: "Une application qui s'adresse aux participants de Klub, pour préparer et prolonger le karma des séances de partage sur un livre.",
+  uvp: "Une application qui s'adresse aux participants d'un klub, pour préparer et prolonger le karma des séances de partage.",
   actors: "Le klubOwner prépare un Klub en s'occupant de la sélection du livre, le klubMaster en fait le compte-rendu, et tout le monde peut proposer des livres.",
   license : "Klub est un logiciel libre, sous licence MIT. Les contributeurs sont les bienvenus",
   framework : "Meteor, un framework open-source de développement web en Javascript basé sur Node.js",

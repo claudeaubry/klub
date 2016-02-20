9358367091c615f5f29bb68ebbf2240c951055ea
claudeaubry <claude.aubry@gmail.com>
2016-02-20 14:08:21 +0100

    fix of the select provoking exception in admin books list

--- packages/pastklub/lib/client/past_klub_edit.js
+++ packages/pastklub/lib/client/past_klub_edit.js
@@ -12,28 +12,25 @@ Template.pastKlubEdit.events({
 
     elt.preventDefault()
     klub.i = +$('input.i').val()
-    klub.klubMaster = $('input.klubMaster').val()
     klub.date = $('input.date').val()
     klub.voteer = $('input.vote').val()
     klub.meetup = $('input.meetup').val()
-    klub.book_id = $('select[name=selBook]').val()
-
     PastKlub.update(_id, klub)
     Router.go('adminPastKlubList')
-  },
-  'click .delete': elt => {
-    const _id = $('input.id').val()
-
-    elt.preventDefault()
-    PastKlub.remove(_id)
-    Router.go('adminPastKlubList')
+  // },
+  // 'click .delete': elt => {
+  //   const _id = $('input.id').val()
+  //
+  //   elt.preventDefault()
+  //   PastKlub.remove(_id)
+  //   Router.go('adminPastKlubList')
   }
 })
 
-Template.pastKlubEdit.rendered = function () {
-  /*
-  Add this method because it works on refresh
-  but won't by clicking on previous list page.
- */
-  this.$('select[name="selBook"]').val(this.$('input.book-id').val())
-}
+// Template.pastKlubEdit.rendered = function () {
+//   /*
+//   Add this method because it works on refresh
+//   but won't by clicking on previous list page.
+//  */
+//   this.$('select[name="selBook"]').val(this.$('input.book-id').val())
+// }

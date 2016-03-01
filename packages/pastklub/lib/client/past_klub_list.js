4d8a9ead62ade2430201eb862385f372dba5c858
claudeaubry <claude.aubry@gmail.com>
2016-03-01 10:17:19 +0100

    tuned labels

--- packages/pastklub/lib/client/past_klub_list.js
+++ packages/pastklub/lib/client/past_klub_list.js
@@ -2,11 +2,11 @@ Template.pastKlubList.helpers({
   pastKlubs: () => {
     const pastKlubs = []
     const pastKlubCollection = PastKlub.find({},
-        {fields: {i: 1, date: 1, book_id: 1}, sort: {i: -1}})
+        {fields: {i: 1, type: 1, date: 1, book_id: 1}, sort: {i: -1}})
 
     pastKlubCollection.map(elt => {
       const title = new Library().bookTitleById(elt.book_id)
-      pastKlubs.push({i: elt.i, date: elt.date, book: title})
+      pastKlubs.push({i: elt.i, type: elt.type, date: elt.date, book: title})
     })
 
     return pastKlubs

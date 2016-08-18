979d268f2e30613a99e42fd3cd609d5ad027f396
claudeaubry <claude.aubry@gmail.com>
2016-08-18 11:39:14 +0200

    refactored frozen

--- packages/book/lib/client/books_frozen.js
+++ packages/book/lib/client/books_frozen.js
@@ -2,18 +2,9 @@ Template.frozen.helpers({
   typeKlub: function() { return Session.get('typeKlub')},
   lecture: function() { return Session.get('typeKlub') === 'lecture' },
   jeu: function() { return Session.get('typeKlub') === 'jeu' },
-  scribing: function() { return Session.get('typeKlub') === 'scribing' }
-})
-
-Template.booksFrozen.helpers({
-  books: () => Books.find({type: 'book', statut: 'frozen'})
-})
-
-Template.gamesFrozen.helpers({
-  games: () => Books.find({type: 'game', statut: 'frozen'})
-})
-
-Template.videosFrozen.helpers({
+  scribing: function() { return Session.get('typeKlub') === 'scribing' },
+  books: () => Books.find({type: 'book', statut: 'frozen'}),
+  games: () => Books.find({type: 'game', statut: 'frozen'}),
   videos: () => Books.find({type: 'video', statut: 'frozen'})
 })
 

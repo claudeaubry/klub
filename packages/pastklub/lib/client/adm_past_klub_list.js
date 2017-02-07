3585686b24d1a37a01f9a6f09c2edde29e773bcf
claudeaubry <claude.aubry@gmail.com>
2017-02-07 18:47:59 +0100

    remis titre dans le tableau des pastklub

--- packages/pastklub/lib/client/adm_past_klub_list.js
+++ packages/pastklub/lib/client/adm_past_klub_list.js
@@ -1,17 +1,16 @@
 Template.pastKlubList.helpers({
   pastKlubs: () => {
-    const pastKlubs = PastKlub.find({}, {sort: {i: -1}})
+    // const pastKlubs = PastKlub.find({}, {sort: {i: -1}})
+    // return pastKlubs
+    const pastKlubs = []
+    const pastKlubCollection = PastKlub.find({},
+        {fields: {i: 1, type: 1, date: 1, book_id: 1}, sort: {i: -1}})
+
+    pastKlubCollection.map(elt => {
+      const title = new Library().bookTitleById(elt.book_id)
+      pastKlubs.push({i: elt.i, type: elt.type, date: elt.date, book: title})
+    })
 
     return pastKlubs
-    // const pastKlubs = []
-    // const pastKlubCollection = PastKlub.find({},
-    //     {fields: {i: 1, type: 1, date: 1, book_id: 1}, sort: {i: -1}})
-    //
-    // pastKlubCollection.map(elt => {
-    //   const title = new Library().bookTitleById(elt.book_id)
-    //   pastKlubs.push({i: elt.i, type: elt.type, date: elt.date, book: title})
-    // })
-    //
-    // return pastKlubs
   }
 })

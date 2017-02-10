50c1d7cc51b520400687ad77c19e453dbedde33e
claudeaubry <claude.aubry@gmail.com>
2017-02-10 07:15:27 +0100

    reactive pour filtrer par type de klub

--- packages/pastklub/lib/client/past_klub_display.js
+++ packages/pastklub/lib/client/past_klub_display.js
@@ -1,21 +1,28 @@
 import { ReactiveDict } from 'meteor/reactive-dict';
 
 Template.pastKlubDisplay.onCreated(function bodyOnCreated() {
-  this.stateLecture = new ReactiveDict();
+  this.klubVisible = new ReactiveDict();
 });
 
 Template.pastKlubDisplay.helpers({
   pastKlubAll() {
   const instance = Template.instance()
-  if (instance.stateLecture.get('onlyLecture')) {
+  if (instance.klubVisible.get('kvisible') === 'lecture') {
     return PastKlub.find({type : 'lecture'}, {sort: {i: -1}})
+  }
+  if (instance.klubVisible.get('kvisible') === 'jeu') {
+    return PastKlub.find({type : 'jeu'}, {sort: {i: -1}})
+  }
+  if (instance.klubVisible.get('kvisible') === 'scribing') {
+    return PastKlub.find({type : 'scribing'}, {sort: {i: -1}})
   }
   return PastKlub.find({}, {sort: {i: -1}})
   }
 })
 
 Template.pastKlubDisplay.events({
-  'change .lecture input'(event, instance) {
-    instance.stateLecture.set('onlyLecture', event.target.checked);
+  'change .klubs input'(event, instance) {
+     const radioValue = $('input[name="pastVisible"]:checked').val()
+    instance.klubVisible.set('kvisible', radioValue);
 }
 })
